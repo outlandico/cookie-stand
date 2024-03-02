bc81a04d9e6bdf079c737e200a5a1def7009be5a
Adam Jimenez <outlandico@gmail.com>
2024-03-01 17:38:42 -0800

    added js file to use getElementby

---
+++ locations.js
@@ -0,0 +1,24 @@
+// Seattle Information
+document.getElementById("seattle-hours").textContent = "Hours Open: 6am-7pm";
+document.getElementById("seattle-contact").textContent = "Contact Info: 123-456-7890";
+document.getElementById("seattle-location").textContent = "Location: 2901 3rd Ave. #300, Seattle, WA 98121";
+
+// Tokyo Information
+document.getElementById("tokyo-hours").textContent = "Hours Open: 6am-7pm";
+document.getElementById("tokyo-contact").textContent = "Contact Info: 222-222-2222";
+document.getElementById("tokyo-location").textContent = "Location: 1 Chrome 1-2 Oshlage, Sumida City, Tokyo 131-8634";
+
+// Dubai Information
+document.getElementById("dubai-hours").textContent = "Hours Open: 6am-7pm";
+document.getElementById("dubai-contact").textContent = "Contact Info: 333-333-3333";
+document.getElementById("dubai-location").textContent = "Location: 1 Sheikh Mohammed bin Rashid Blvd-Dubai";
+
+// Paris Information
+document.getElementById("paris-hours").textContent = "Hours Open: 6am-7pm";
+document.getElementById("paris-contact").textContent = "Contact Info: 444-444-4444";
+document.getElementById("paris-location").textContent = "Location: Champ de Mars, 5 Avenue Anatole France, 75007 Paris";
+
+// Lima Information
+document.getElementById("lima-hours").textContent = "Hours Open: 6am-7pm";
+document.getElementById("lima-contact").textContent = "Contact Info: 555-555-5555";
+document.getElementById("lima-location").textContent = "Location: Ca. Gral. Borgoño cuadra 8, Miraflores 15074";
